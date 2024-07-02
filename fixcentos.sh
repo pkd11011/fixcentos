2c96ea63adb223fb9454e07a03053d4e62fa1ac7
pkd11011 <130068719+pkd11011@users.noreply.github.com>
2024-07-02 22:14:05 +0700

    Create fixcentos.sh

---
+++ fixcentos.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Backup the existing repo file
+cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+
+# Create a new CentOS-Base.repo file with the updated content
+cat <<EOL > /etc/yum.repos.d/CentOS-Base.repo
+[base]
+name=CentOS-\$releasever - Base
+baseurl=http://vault.centos.org/7.9.2009/os/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[updates]
+name=CentOS-\$releasever - Updates
+baseurl=http://vault.centos.org/7.9.2009/updates/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[extras]
+name=CentOS-\$releasever - Extras
+baseurl=http://vault.centos.org/7.9.2009/extras/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[centosplus]
+name=CentOS-\$releasever - Plus
+baseurl=http://vault.centos.org/7.9.2009/centosplus/\$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOL
+
+# Clean yum cache and make cache
+yum clean all
+yum makecache
+
+# Install PHP and necessary modules
+yum install php php-cli php-fpm php-mysqlnd php-xml php-mbstring php-json php-gd php-curl -y
+
+echo "Repository updated and PHP installed successfully."
